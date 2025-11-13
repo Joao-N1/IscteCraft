45b5835cbb4d97ea77435b8ef8c72c0614c5eab8
JoãoNeves <jmpns2@iscte-iul.pt>
2025-11-13 16:21:12 +0000

    Grass Block and Dirt Block

---
+++ src/main/java/jogo/voxel/blocks/DirtBlockType.java
@@ -0,0 +1,25 @@
+package jogo.voxel.blocks;
+
+import com.jme3.asset.AssetManager;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.texture.Texture2D;
+import jogo.util.ProcTextures;
+import jogo.voxel.VoxelBlockType;
+
+public class DirtBlockType extends VoxelBlockType {
+        public DirtBlockType() {super("dirt");}
+
+    @Override
+    public Material getMaterial(AssetManager assetManager) {
+        Texture2D tex = (Texture2D) assetManager.loadTexture("Textures/DirtBlock.png");
+        Material m = new Material(assetManager, "Common/MatDefs/Light/Lighting.j3md");
+        m.setTexture("DiffuseMap", tex);
+        m.setBoolean("UseMaterialColors", true);
+        m.setColor("Diffuse", ColorRGBA.White);
+        m.setColor("Specular", ColorRGBA.White.mult(0.03f));
+        m.setFloat("Shininess", 8f);
+        return m;
+    }
+}
+
