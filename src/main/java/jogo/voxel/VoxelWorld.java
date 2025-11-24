b96569fcb9f6c156aebc2bad615c64c815344f8e
Rodrigo Lopes <ralls@iscte-iul.pt>
2025-11-24 16:34:39 +0000

    Decrease tree generation frequency for balanced gameplay and spiky trees generation added

--- src/main/java/jogo/voxel/VoxelWorld.java
+++ src/main/java/jogo/voxel/VoxelWorld.java
@@ -167,7 +167,7 @@ public class VoxelWorld {
 
         Random random = new Random(seed + 12345); // usa seed global para árvores consistentes
 
-        int treeChance = 300; // 1 em 200 blocos gera uma árvore
+        int treeChance = 400; // 1 em 400 blocos gera uma árvore
         int minHeight = 4;   // altura mínima do tronco
         int maxHeight = 6;   // altura máxima do tronco
 
@@ -190,6 +190,58 @@ public class VoxelWorld {
                 // TRONCO
                 for (int h = 1; h <= treeHeight; h++) {
                     setBlock(x, y + h, z, VoxelPalette.Wood_ID);
+                }
+
+                // FOLHAS
+                int top = y + treeHeight;
+
+                for (int lx = -2; lx <= 2; lx++) {
+                    for (int lz = -2; lz <= 2; lz++) {
+                        for (int ly = 0; ly <= 2; ly++) {
+
+                            // deixa algumas folhas com buracos aleatórios
+                            if (random.nextInt(6) == 0) continue;
+
+                            setBlock(
+                                    x + lx,
+                                    top + ly,
+                                    z + lz,
+                                    VoxelPalette.Leaf_ID
+                            );
+                        }
+                    }
+                }
+            }
+        }
+        // ===========================================
+        //  GERAÇÃO DE ÁRVORES COM ESPINHOS
+        // ===========================================
+
+        random = new Random(seed + 12345);
+
+        treeChance = 1000; // 1 em 1000 blocos gera uma árvore
+        minHeight = 4;
+        maxHeight = 6;
+
+        for (int x = 2; x < sizeX - 2; x++) {
+            for (int z = 2; z < sizeZ - 2; z++) {
+
+                // chance aleatória de gerar árvore
+                if (random.nextInt(treeChance) != 0) continue;
+
+                // obter altura do solo
+                int y = getTopSolidY(x, z);
+                if (y < 0) continue;
+
+                // só gerar em cima de GRASS
+                if (getBlock(x, y, z) != VoxelPalette.GRASS_ID) continue;
+
+                // altura da árvore
+                int treeHeight = minHeight + random.nextInt(maxHeight - minHeight + 1);
+
+                // TRONCO
+                for (int h = 1; h <= treeHeight; h++) {
+                    setBlock(x, y + h, z, VoxelPalette.SpikyWood_ID);
                 }
 
                 // FOLHAS
